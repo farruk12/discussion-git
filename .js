917d08d34bf8d23e105113d4ced4b4d1e0801d97
Omar Farouq Abdi <127293288+farruk12@users.noreply.github.com>
2023-08-26 23:08:19 +0300

    Update .js added something

--- .js
+++ .js
@@ -2,4 +2,6 @@ const  name = "javascript is awesome";
 function gameMode(userMove) {
     const randomNumber = Math.random();
     let computerMove = "";
+    if(randomNumber >= 0 && randomNumber < 1/3) {
+        computerMove = ';;
 }
